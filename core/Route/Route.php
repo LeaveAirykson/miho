50ba45c52aac844c4f86f8b2fa9c1f5c9094ed91
Geeg <georg.boete@gmail.com>
2024-05-30 23:28:24 +0200

    Integrate RouteHandler into Route class

--- core/Route/Route.php
+++ core/Route/Route.php
@@ -2,15 +2,103 @@
 
 namespace App\Core\Route;
 
+use App\Core\Utility\ControllerResolver;
+
 class Route
 {
+    private string $method;
+    private string $path;
+    private $controller;
+    private ?string $action = null;
+    private array $guard = [];
+    private array $params = [];
 
-    public $route;
-    public $params;
-
-    function __construct()
+    function __construct($method, $path, $controller, array $guard = [])
     {
-        $this->params = null;
-        $this->route = null;
+        $this->setMethod($method);
+        $this->setPath($path);
+        $this->setController($controller);
+        $this->setGuard($guard);
+
+        return $this;
+    }
+
+    function setMethod(string $method)
+    {
+        $this->method = trim(strtoupper($method));
+        return $this;
+    }
+
+    function getMethod()
+    {
+        return $this->method;
+    }
+
+    function setController(string $controller)
+    {
+        $parts = explode('::', $controller);
+        $this->controller = ControllerResolver::resolve($parts[0]);
+        $this->action = $parts[1] ?? $this->action;
+        return $this;
+    }
+
+    function getController()
+    {
+        return $this->controller;
+    }
+
+    function setAction(string $action)
+    {
+        $this->action = $action;
+        return $this;
+    }
+
+    function getAction()
+    {
+        return $this->action;
+    }
+
+    function setParams(?array $params = null)
+    {
+        $this->params = $params;
+        return $this;
+    }
+
+    function getParams()
+    {
+        return $this->params;
+    }
+
+    function setParam(string $name, $value)
+    {
+        $this->params[$name] = $value;
+        return $this;
+    }
+
+    function getParam(string $name)
+    {
+        return $this->params[$name] ?? null;
+    }
+
+    function setGuard(?array $guard)
+    {
+        $this->guard = $guard;
+        return $this;
+    }
+
+    function getGuard()
+    {
+        return $this->guard;
+    }
+
+    function setPath(string $path)
+    {
+        $this->path = $path;
+        return $this;
+    }
+
+    function getPath()
+    {
+        return $this->path;
     }
 }
